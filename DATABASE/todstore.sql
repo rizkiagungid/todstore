d85a1e0b7641d351ef1ac2050408a210ab9f6f5b
Rizki Agung Sentosa <mrrizkiagung@gmail.com>
2022-10-13 10:25:11 +0700

    DATABASE UPDATE
    DATABASE UPDATE TODSTORE

--- DATABASE/todstore.sql
+++ DATABASE/todstore.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2022 at 04:29 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: Oct 13, 2022 at 05:23 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `penjualan`
+-- Database: `todstore`
 --
 
 -- --------------------------------------------------------
@@ -29,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `produk` (
   `id` int(11) NOT NULL,
-  `nama_produk` varchar(500) NOT NULL,
+  `name` varchar(500) NOT NULL,
   `model` varchar(500) NOT NULL,
   `harga` varchar(500) NOT NULL,
   `stok` varchar(500) NOT NULL,
@@ -41,30 +42,33 @@ CREATE TABLE `produk` (
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id`, `nama_produk`, `model`, `harga`, `stok`, `ukuran`, `image`) VALUES
-(1, 'TOD Japanesee', 'MEN-711', '92000', '2', 'L', 'b1d96f6fccdf017f53c0dd697d25fced.jpg');
+INSERT INTO `produk` (`id`, `name`, `model`, `harga`, `stok`, `ukuran`, `image`) VALUES
+(1, 'TOD Japanesee', 'MEN-711', '92000', '2', 'L', 'b1d96f6fccdf017f53c0dd697d25fced.jpg'),
+(2, 'Batik Olympus TOD', 'Limited-edition-TOD69', '15000', '3', 'L', 'LINE_ALBUM_2933-2936-TNW 112146-2933-2936-SP_220108_8-1279x1280.jpg'),
+(3, 'IndoPride TOD Jacket', 'MEN-J998', '69000', '6', 'XL', 'normal_69234ea215.jpg'),
+(4, 'Tzuyu Edition', 'Limited-edition-TOD70', '54000', '11', 'S', '2948649_f6626da9-da33-4d9e-a5e8-dfd9885aa069_700_700.jpg'),
+(5, 'TOD Wiboee Style', 'WOMEN-6969', '15000', '7', 'M', '07susi.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `student` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(191) NOT NULL,
-  `model_produk` varchar(191) NOT NULL,
-  `harga` varchar(191) NOT NULL,
-  `image` varchar(191) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `student`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `student` (`id`, `name`, `model_produk`, `harga`, `image`) VALUES
-(1, 'Baju Hitam TOD', 'MEN', '192.000', 'b1d96f6fccdf017f53c0dd697d25fced.jpg'),
-(2, 'Baju Blackpink', 'WOMEN', '392.000', '5d0af0beaba21e1a242e09ce1e5fc899.jpg');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(2, 'rizkiagungid', 'mrrizkiagung@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(3, 'ganteng', 'ganteng@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
@@ -77,9 +81,9 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `student`
+-- Indexes for table `users`
 --
-ALTER TABLE `student`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -90,12 +94,12 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `student`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `student`
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 

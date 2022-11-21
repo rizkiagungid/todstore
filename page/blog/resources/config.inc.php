90b10a97a566959a478fdce17333be77654ceee3
Rizki Agung Sentosa <mrrizkiagung@gmail.com>
2022-11-21 11:11:23 +0700

    UPDATE

--- page/blog/resources/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['db_host']  = 'localhost';
-$config['db_user']  = 'root';
-$config['db_pass']  = '';
-$config['db_name']  = 'blog';
-
-foreach($config as $k => $v){
-    define(strtoupper($k),$v);
-}
